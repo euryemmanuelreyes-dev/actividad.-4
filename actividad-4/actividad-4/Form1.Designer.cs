04f404debb74272c3150d20dec7ac0a0759b47a9
euryemmanuelreyes-dev <euryemmanuelreyes@gmail.com>
2025-10-30 00:58:59 -0400

    Create Form1.Designer.cs

---
+++ actividad-4/actividad-4/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace actividad_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            menuDeUsuariosToolStripMenuItem = new ToolStripMenuItem();
+            usariosToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { menuDeUsuariosToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 33);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // menuDeUsuariosToolStripMenuItem
+            // 
+            menuDeUsuariosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { usariosToolStripMenuItem });
+            menuDeUsuariosToolStripMenuItem.Name = "menuDeUsuariosToolStripMenuItem";
+            menuDeUsuariosToolStripMenuItem.Size = new Size(108, 29);
+            menuDeUsuariosToolStripMenuItem.Text = "opcciones";
+            // 
+            // usariosToolStripMenuItem
+            // 
+            usariosToolStripMenuItem.Name = "usariosToolStripMenuItem";
+            usariosToolStripMenuItem.Size = new Size(270, 34);
+            usariosToolStripMenuItem.Text = "usarios";
+            usariosToolStripMenuItem.Click += usariosToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "}";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem menuDeUsuariosToolStripMenuItem;
+        private ToolStripMenuItem usariosToolStripMenuItem;
+    }
+}
