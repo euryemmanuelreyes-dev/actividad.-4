1a92c6782ebfe7d4318299a5fdcab554fae21608
euryemmanuelreyes-dev <euryemmanuelreyes@gmail.com>
2025-10-30 00:59:13 -0400

    Create FormNombre.Designer.cs

---
+++ actividad-4/actividad-4/FormNombre.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace actividad_4
+{
+    partial class Formusuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LblNombre = new Label();
+            txtNombre = new TextBox();
+            lblApellido = new Label();
+            label3 = new Label();
+            lblDIRECC = new Label();
+            LBLfechaDN = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            txtApellido = new TextBox();
+            txtDni = new TextBox();
+            txtDirecc = new TextBox();
+            txtSexo = new TextBox();
+            txtFirma = new TextBox();
+            txtNacionalidad = new TextBox();
+            txtEC = new TextBox();
+            txtPFE = new TextBox();
+            txtTDSangre = new TextBox();
+            dtpFechaDN = new DateTimePicker();
+            dtpFechaDV = new DateTimePicker();
+            dgvAlmacenar = new DataGridView();
+            btnGuardar = new Button();
+            btnEliminar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvAlmacenar).BeginInit();
+            SuspendLayout();
+            // 
+            // LblNombre
+            // 
+            LblNombre.AutoSize = true;
+            LblNombre.Location = new Point(12, 19);
+            LblNombre.Name = "LblNombre";
+            LblNombre.Size = new Size(78, 25);
+            LblNombre.TabIndex = 0;
+            LblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(103, 12);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(150, 31);
+            txtNombre.TabIndex = 1;
+            // 
+            // lblApellido
+            // 
+            lblApellido.AutoSize = true;
+            lblApellido.Location = new Point(12, 75);
+            lblApellido.Name = "lblApellido";
+            lblApellido.Size = new Size(78, 25);
+            lblApellido.TabIndex = 2;
+            lblApellido.Text = "Apellido";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 130);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 25);
+            label3.TabIndex = 3;
+            label3.Text = "DNI";
+            // 
+            // lblDIRECC
+            // 
+            lblDIRECC.AutoSize = true;
+            lblDIRECC.Location = new Point(12, 183);
+            lblDIRECC.Name = "lblDIRECC";
+            lblDIRECC.Size = new Size(85, 25);
+            lblDIRECC.TabIndex = 4;
+            lblDIRECC.Text = "Direccion";
+            // 
+            // LBLfechaDN
+            // 
+            LBLfechaDN.AutoSize = true;
+            LBLfechaDN.Location = new Point(12, 230);
+            LBLfechaDN.Name = "LBLfechaDN";
+            LBLfechaDN.Size = new Size(174, 25);
+            LBLfechaDN.TabIndex = 5;
+            LBLfechaDN.Text = "Fecha de nacimiento";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 286);
+            label6.Name = "label6";
+            label6.Size = new Size(48, 25);
+            label6.TabIndex = 6;
+            label6.Text = "sexo";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(529, 15);
+            label7.Name = "label7";
+            label7.Size = new Size(56, 25);
+            label7.TabIndex = 7;
+            label7.Text = "Firma";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(483, 64);
+            label8.Name = "label8";
+            label8.Size = new Size(115, 25);
+            label8.TabIndex = 8;
+            label8.Text = "Nacionalidad";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(485, 118);
+            label9.Name = "label9";
+            label9.Size = new Size(100, 25);
+            label9.TabIndex = 9;
+            label9.Text = "Estado civil";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(498, 171);
+            label10.Name = "label10";
+            label10.Size = new Size(87, 25);
+            label10.TabIndex = 10;
+            label10.Text = "Profecion";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(468, 230);
+            label11.Name = "label11";
+            label11.Size = new Size(130, 25);
+            label11.TabIndex = 11;
+            label11.Text = "Tipo de sangre";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(415, 286);
+            label12.Name = "label12";
+            label12.Size = new Size(183, 25);
+            label12.TabIndex = 12;
+            label12.Text = "Fecha de vencimiento";
+            // 
+            // txtApellido
+            // 
+            txtApellido.Location = new Point(103, 72);
+            txtApellido.Name = "txtApellido";
+            txtApellido.Size = new Size(150, 31);
+            txtApellido.TabIndex = 13;
+            // 
+            // txtDni
+            // 
+            txtDni.Location = new Point(103, 124);
+            txtDni.Name = "txtDni";
+            txtDni.Size = new Size(150, 31);
+            txtDni.TabIndex = 14;
+            // 
+            // txtDirecc
+            // 
+            txtDirecc.Location = new Point(103, 177);
+            txtDirecc.Name = "txtDirecc";
+            txtDirecc.Size = new Size(150, 31);
+            txtDirecc.TabIndex = 15;
+            // 
+            // txtSexo
+            // 
+            txtSexo.Location = new Point(103, 288);
+            txtSexo.Name = "txtSexo";
+            txtSexo.Size = new Size(150, 31);
+            txtSexo.TabIndex = 17;
+            // 
+            // txtFirma
+            // 
+            txtFirma.Location = new Point(628, 12);
+            txtFirma.Name = "txtFirma";
+            txtFirma.Size = new Size(150, 31);
+            txtFirma.TabIndex = 18;
+            // 
+            // txtNacionalidad
+            // 
+            txtNacionalidad.Location = new Point(628, 58);
+            txtNacionalidad.Name = "txtNacionalidad";
+            txtNacionalidad.Size = new Size(150, 31);
+            txtNacionalidad.TabIndex = 19;
+            // 
+            // txtEC
+            // 
+            txtEC.Location = new Point(628, 112);
+            txtEC.Name = "txtEC";
+            txtEC.Size = new Size(150, 31);
+            txtEC.TabIndex = 20;
+            // 
+            // txtPFE
+            // 
+            txtPFE.Location = new Point(628, 165);
+            txtPFE.Name = "txtPFE";
+            txtPFE.Size = new Size(150, 31);
+            txtPFE.TabIndex = 21;
+            // 
+            // txtTDSangre
+            // 
+            txtTDSangre.Location = new Point(628, 222);
+            txtTDSangre.Name = "txtTDSangre";
+            txtTDSangre.Size = new Size(150, 31);
+            txtTDSangre.TabIndex = 22;
+            // 
+            // dtpFechaDN
+            // 
+            dtpFechaDN.Location = new Point(192, 222);
+            dtpFechaDN.Name = "dtpFechaDN";
+            dtpFechaDN.Size = new Size(244, 31);
+            dtpFechaDN.TabIndex = 24;
+            // 
+            // dtpFechaDV
+            // 
+            dtpFechaDV.Location = new Point(628, 286);
+            dtpFechaDV.Name = "dtpFechaDV";
+            dtpFechaDV.Size = new Size(300, 31);
+            dtpFechaDV.TabIndex = 25;
+            // 
+            // dgvAlmacenar
+            // 
+            dgvAlmacenar.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvAlmacenar.Location = new Point(12, 325);
+            dgvAlmacenar.Name = "dgvAlmacenar";
+            dgvAlmacenar.RowHeadersWidth = 62;
+            dgvAlmacenar.Size = new Size(1198, 326);
+            dgvAlmacenar.TabIndex = 26;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(875, 15);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(112, 49);
+            btnGuardar.TabIndex = 27;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(876, 94);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(112, 49);
+            btnEliminar.TabIndex = 28;
+            btnEliminar.Text = "limpiar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // Formusuarios
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1222, 663);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnGuardar);
+            Controls.Add(dgvAlmacenar);
+            Controls.Add(dtpFechaDV);
+            Controls.Add(dtpFechaDN);
+            Controls.Add(txtTDSangre);
+            Controls.Add(txtPFE);
+            Controls.Add(txtEC);
+            Controls.Add(txtNacionalidad);
+            Controls.Add(txtFirma);
+            Controls.Add(txtSexo);
+            Controls.Add(txtDirecc);
+            Controls.Add(txtDni);
+            Controls.Add(txtApellido);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(LBLfechaDN);
+            Controls.Add(lblDIRECC);
+            Controls.Add(label3);
+            Controls.Add(lblApellido);
+            Controls.Add(txtNombre);
+            Controls.Add(LblNombre);
+            Name = "Formusuarios";
+            Text = "susarios";
+            Load += Formusuarios_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvAlmacenar).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label LblNombre;
+        private TextBox txtNombre;
+        private Label lblApellido;
+        private Label label3;
+        private Label lblDIRECC;
+        private Label LBLfechaDN;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private TextBox txtApellido;
+        private TextBox txtDni;
+        private TextBox txtDirecc;
+        private TextBox txtSexo;
+        private TextBox txtFirma;
+        private TextBox txtNacionalidad;
+        private TextBox txtEC;
+        private TextBox txtPFE;
+        private TextBox txtTDSangre;
+        private DateTimePicker dtpFechaDN;
+        private DateTimePicker dtpFechaDV;
+        private DataGridView dgvAlmacenar;
+        private Button btnGuardar;
+        private Button btnEliminar;
+    }
+}
