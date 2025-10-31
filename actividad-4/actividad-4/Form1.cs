7e4fc55f8d6b9b55472477aab8e67d1ed5682600
euryemmanuelreyes-dev <euryemmanuelreyes@gmail.com>
2025-10-31 00:07:07 -0400

    Update Form1.cs

--- actividad-4/actividad-4/Form1.cs
+++ actividad-4/actividad-4/Form1.cs
@@ -14,5 +14,19 @@ namespace actividad_4
             formusuarios.MdiParent = this;
             formusuarios.Show();
         }
+
+        private void divisasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            divisas formdivisas = new divisas();
+            formdivisas.MdiParent = this;
+            formdivisas.Show();
+        }
+
+        private void fibonacciToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fibonacci formfibonacci = new fibonacci();
+            formfibonacci.MdiParent = this;
+            formfibonacci.Show();
+        }
     }
 }
