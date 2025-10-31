51ce1d7ea5c9794b0bac88641011cef1eb1c1a7b
euryemmanuelreyes-dev <euryemmanuelreyes@gmail.com>
2025-10-31 00:06:45 -0400

    Create fibonacci.cs

---
+++ actividad-4/actividad-4/fibonacci.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace actividad_4
+{
+    public partial class fibonacci : Form
+    {
+        public fibonacci()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtlimite.Text, out int limite) || limite <= 0)
+            {
+                MessageBox.Show("ingrse un limite", "aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Crear un StringBuilder para construir la serie
+            StringBuilder serie = new StringBuilder();
+            int a = 0, b = 1;
+
+            serie.Append($"{a}");
+
+            while (b <= limite)
+
+            {
+                serie.Append($", {b}");
+                int temp = a + b;
+                a = b;
+                b = temp;
+            }
+                    txtresultado.Text = serie.ToString();
+                }
+            }
+        }
+    
+            
